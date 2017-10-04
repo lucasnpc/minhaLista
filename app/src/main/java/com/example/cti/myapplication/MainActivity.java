44cada5f9d731ffe46ec258856c9e96f134ca6bc
lucasnpc <31970919+lucasnpc@users.noreply.github.com>
2017-10-04 16:25:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cti/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNome = (EditText) findViewById(R.id.editText);
+    }
+
+    public void limpaNome(View view){
+        String texto = "";
+        txtNome.setText(texto);
+    }
+}
